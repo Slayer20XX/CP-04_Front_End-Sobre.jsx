57b0b38792457c871c32a12813b86fe704989828
Slayer20XX <m.taylor@outlook.com.br>
2024-09-07 19:41:49 -0300

    CP-04-FrontEnd-Sobre.jsx

---
+++ Sobre.jsx
@@ -0,0 +1,13 @@
+const Sobre=()=>{
+    return(
+        <>
+            <h1>Sobre</h1>
+            <h3>
+                Encontre uma seleção incrível de artigos esportivos para todos os gostos e níveis. Desde roupas e acessórios para caminhada, corrida e ciclismo até equipamentos para esportes radicais, temos tudo que você precisa para levar seu desempenho ao próximo nível.
+                Além disso, fique por dentro das últimas notícias do mundo dos esportes, incluindo dicas, lançamentos de produtos e cobertura de eventos. Se você procura aventura, performance e qualidade, este é o lugar para você!
+                Explore, inspire-se e experimente a aventura!
+            </h3>
+        </>
+    )
+}
+export default Sobre
